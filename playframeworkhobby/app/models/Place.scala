e48d8eb3dc4044439070f4f13c55862f64070c2e
shivalasvegas <shiva.king@academytrainee.com>
2020-09-25 20:46:12 +0100

    SK - Place.scala: created and edited with case classes

--- playframeworkhobby/app/models/Place.scala
+++ playframeworkhobby/app/models/Place.scala
@@ -1,5 +1,12 @@
 package models
 
-class Place {
+case class PlaceData(id: Option[Int], name: String, description: String)
+
+case class Place(id: Option[Int], name: String, description: String)
+
+object Place{
 
 }
+
+
+
