e6b6b10c0d59914b80fcbaf7004dfc1e79cb8f04
shivalasvegas <shiva.king@academytrainee.com>
2020-10-02 12:36:20 +0100

    SK - refactored for connection to database

--- playframeworkhobby/app/application/PlacesApplication.scala
+++ playframeworkhobby/app/application/PlacesApplication.scala
@@ -1,16 +1,15 @@
 package application
 
-import daos.{PlaceDAO, PlaceDocumentDAO}
-import models.{Place, PlaceData, PlaceDocument}
+import models.{Place}
 import reactivemongo.api.bson.collection.BSONCollection
-import reactivemongo.api.bson.{BSONDocumentReader, BSONDocumentWriter, BSONObjectID, Macros}
+import reactivemongo.api.bson.{BSONDocumentReader, BSONDocumentWriter, Macros}
 import reactivemongo.api.commands.WriteResult
 import reactivemongo.api._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-object PlacesApplication extends App {
+object PlacesApplication {
 
   import ExecutionContext.Implicits.global
 
@@ -28,111 +27,23 @@ object PlacesApplication extends App {
   db1.onComplete {
     case Success(successMessage) => println(s"Connected to $successMessage")
     case Failure(failureMessage) => println(failureMessage)
-//    case resolution => println(s"DB resolution ... $resolution")
-//      driver.close()
+
   }
 
   def collectionPlace: Future[BSONCollection] = db1.map(_.collection("place"))
-  def collectionDoc: Future[BSONCollection] = db1.map(_.collection("placedoc"))
 
   implicit def placesWriter: BSONDocumentWriter[Place] = Macros.writer[Place]
-  implicit def placesDocWriter: BSONDocumentWriter[PlaceDocument] = Macros.writer[PlaceDocument]
   implicit def placesReader: BSONDocumentReader[Place] = Macros.reader[Place]
 
-  def create(placeData: PlaceData): Future[WriteResult] = {
-    val place = Place(BSONObjectID.generate().stringify, placeData.name, placeData.description)
+  def createPlaceWithForm(place: Place): Future[Unit] =  {
 
-    for {
-      places <- collectionPlace
-      writeResult <- places.insert.one(place)
-    } yield writeResult
-
+    val writeRes: Future[WriteResult] = collectionPlace.flatMap(_.insert.one(place))
+    writeRes.onComplete {
+            case Failure(failureMessage) => println(failureMessage)
+            case Success(writeResult) =>
+              println(s"successfully inserted document from post with result: $writeResult")
+          }
+          writeRes.map(_ => {})
   }
-  // Get this value from webpage
-//  val placeDocument = new PlaceDocument(22, "West Lulworth", "Gateway to the fossil Forest")
-//
-//  println("Waiting ...")
-//
-//  def createWithCollectionDoc(collection: BSONCollection, placeDocument: PlaceDocument): Future[Unit] = Future {
-//    println(s"Database connection established ... $placeDocument")
-//
-//    val writeRes: Future[WriteResult] = collection.insert.one(placeDocument)
-//
-//    writeRes.onComplete {
-//      case Failure(e) => e.printStackTrace()
-//      case Success(writeResult) =>
-//        println(s"successfully inserted document with result: $writeResult")
-//    }
-//
-//    writeRes.map(_ => {})
-//  }
-//
-//  collectionDoc onComplete {
-//    case Success(collectionDoc: BSONCollection) =>
-//      PlacesApplication.createWithCollectionDoc(collectionDoc, placeDocument)
-//    case Failure(failureMessage) => println(failureMessage)
-//  }
-
-
-
-//    def createPlaceWithForm(place: Place): Future[Unit] =  Future {
-//      println(s"Creating document ... $place ")
-//      def createWithCollection(collection: BSONCollection, place: Place): Future[Unit] = Future {
-//        println(s"Database connection established ... $place")
-//
-//        val writeRes: Future[WriteResult] = collection.insert.one(place)
-//
-//        writeRes.onComplete {
-//          case Failure(e) => e.printStackTrace()
-//          case Success(writeResult) =>
-//            println(s"successfully inserted document with result: $writeResult")
-//        }
-//
-//        writeRes.map(_ => {})
-//
-//      }
-//
-////      collectionPlace onComplete {
-////        case Success(collection: BSONCollection) =>
-////          createWithCollection(collection, place)
-////        case Failure(failureMessage) => println(failureMessage)
-////      }
-////
-//    }
-
-//  def createPlaceWithForm(place: Place): Future[Unit] =  {
-//
-//    println(s"Mapping the data ... $place ")
-//    val writeRes: Future[WriteResult] = collectionPlace.flatMap(_.insert.one(place))
-//    println("Future written ...")
-//
-//    writeRes.onComplete {
-//            case Failure(failureMessage) => println(failureMessage)
-//            case Success(writeResult) =>
-//              println(s"successfully inserted document from post with result: $writeResult")
-//          }
-//          writeRes.map(_ => {})
-//  }
-//  collectionPlace onComplete {
-//    case Success(collectionPlace: BSONCollection) => println(s"Collection $collectionPlace ... created")
-//    case Failure(failureMessage) => println(failureMessage)
-//     }
-//  def createPlaceWithForm(place: Place): Future[Unit] =  Future {
-//
-//    println(s"Mapping the data ... $place ")
-//    val writeRes: Future[WriteResult] = collection.insert.one(place)
-//    val isComplete = writeRes.isCompleted
-//    if (isComplete) println("Inserted document") else println("Oh snap!")
-//    writeRes.onComplete {
-//      case Failure(e) => e.printStackTrace()
-//      case Success(writeResult) =>
-//        println(s"successfully inserted document from post with result: $writeResult")
-//    }
-//    writeRes.map(_ => {})
-//
-//
-//  }
-
-
 
 }
--- playframeworkhobby/app/controllers/ApplicationController.scala
+++ playframeworkhobby/app/controllers/ApplicationController.scala
@@ -4,11 +4,9 @@ import javax.inject.Singleton
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
 import application.PlacesApplication
-import application.PlacesApplication.{collectionPlace, db1}
 import daos.PlaceDAO
 import models.Place
-import reactivemongo.api.bson.collection.BSONCollection
-import reactivemongo.api.bson.{BSONDocumentWriter, BSONObjectID, Macros}
+import reactivemongo.api.bson.{ BSONObjectID }
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -16,53 +14,23 @@ import scala.concurrent.Future
 @Singleton
 class ApplicationController @Inject() (cc: ControllerComponents) extends AbstractController(cc)  with play.api.i18n.I18nSupport {
 
-  PlacesApplication
-
   def uploadPlace(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    implicit def placesWriter: BSONDocumentWriter[Place] = Macros.writer[Place]
 
-    println("Ready to upload ... ")
+    println("Creating document for database ... ")
     val placeData = PlaceDAO.createPlaceForm.bindFromRequest().get
     val place = Place(BSONObjectID.generate().stringify, placeData.name, placeData.description)
-    println("Creating document for database ... ")
 
-    //PlacesApplication.create(placeData)
-    if (db1 == null) println(s"Found null database $db1")
-
-    def collectionPlace =
-      try {
-        db1.map(_.collection("place"))
-      } catch{
-        case ex: NullPointerException => {
-        println("Found the null pointer exception")
-      }
-  }
-    println(s"Writing document for database ... ${placeData.description}")
-    def collectionTest: Future[BSONCollection] = db1.map(_.collection("testplace"))
-    println("Writing document for database ... ")
-    //PlacesApplication.createPlaceWithForm(newPlace)
-
-
-    for {
-      places <- collectionTest
-      writeResult <- places.insert.one(place)
-    } yield writeResult
-
+    PlacesApplication.createPlaceWithForm(place)
     println("Document written ... ")
-    //collectionPlace.flatMap(_.insert.one(newPlace))
-      Future(Ok(views.html.formtest2(PlaceDAO.createPlaceForm)))
-   // println("Should have created document inside collection ...")
-    //Future(Ok(views.html.))
+    Future(Ok(views.html.formtest2(PlaceDAO.createPlaceForm)))
   }
 
   def formtest2(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.formtest2(PlaceDAO.createPlaceForm))
   }
 
-
   def titleParam() = Action { _ =>
     Ok("Now we're cooking on gas")
   }
-
 
 }
